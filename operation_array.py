85876bacd8e8c2dbe3b84cf1b86156dcd4f641f1
Amit Amrutiya <90676830+20CS003@users.noreply.github.com>
2022-04-04 22:55:00 +0530

    Update operation_array.py

--- operation_array.py
+++ operation_array.py
@@ -3,35 +3,35 @@ import numpy as np
 arr = np.array([1, 2, 3, 4, 5, 6, 7])
 arr2 = np.array([13, 12, 11, 10, 9, 8])
 
-# How to access elemnt by index ?
+#  access elemnt by index 
 print('---------------------------------------')
 print("print element of array :")
 print(arr[5])
 
-# How to slice an array ?
+#  slice an array 
 print('---------------------------------------')
 print("Slice of an array :")
 print(arr[1:5])
 print(arr[1:5:2])
 
-# How to know the data type of array ?
+# data type of array 
 print('---------------------------------------')
 print("Data type of an array :")
 print(arr.dtype)
 
-# How to create a copy of an array ?
+#  create a copy of an array 
 x = arr.copy()  # create a copy of an variable
 
-# How to create a view of an array ?
+# create a view of an array 
 y = arr.view()  # create a reference of an variable
 
-# How to merge two array in one ?
+#  merge two array in one 
 print('---------------------------------------')
 temp = np.concatenate((arr, arr2))
 print("Printing merge array :")
 print(temp)
 
-# How to split an array ?
+# split an array 
 print('---------------------------------------')
 print("Split an array into parts :")
 newarr = np.array_split(arr2, 3)

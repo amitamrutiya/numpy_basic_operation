23a61bb6567a9f8891a351346ab64bca0251d4b3
Amit Amrutiya <90676830+20CS003@users.noreply.github.com>
2022-04-04 23:09:58 +0530

    Update operation_array.py

--- operation_array.py
+++ operation_array.py
@@ -4,18 +4,18 @@ arr = np.array([1, 2, 3, 4, 5, 6, 7])
 arr2 = np.array([13, 12, 11, 10, 9, 8])
 
 #  access elemnt by index 
-print('---------------------------------------')
+print('------------------------------------------------------------')
 print("print element of array :")
 print(arr[5])                                # 6
 
 #  slice an array 
-print('---------------------------------------')
+print('------------------------------------------------------------')
 print("Slice of an array :")
 print(arr[1:5])                         # 2 3 4 5
 print(arr[1:5:2])                       # 2 4
 
 # data type of array 
-print('---------------------------------------')
+print('------------------------------------------------------------')
 print("Data type of an array :")
 print(arr.dtype)                        #int32
 
@@ -25,18 +25,18 @@ x = arr.copy()
 y = arr.view()  
 
 #  merge two array in one       
-print('---------------------------------------')
+print('------------------------------------------------------------')
 temp = np.concatenate((arr, arr2))
 print("Printing merge array :")
 print(temp)              # 1 2 3 4 5 6 7 13 12 11 10 9 8
 
 # split an array 
-print('---------------------------------------')
+print('------------------------------------------------------------')
 print("Split an array into parts :")
 newarr = np.array_split(arr2, 3)
 print(newarr)           # array([13,12]) , array([11,10]) , array ([9,8])
 
 # Sort an array
-print('---------------------------------------')
+print('------------------------------------------------------------')
 print("Sorted array :")
 print(np.sort(arr2))     # 8 9 10 11 12 13

6f83433e1506716033632ed7abecfc611d68c8fe
Amit Amrutiya <90676830+20CS003@users.noreply.github.com>
2022-04-04 22:57:16 +0530

    Update create_array.py

--- create_array.py
+++ create_array.py
@@ -1,32 +1,33 @@
 import numpy as np
 
-#How to create array ?
+# create array 
 a = np.array(42)  #0 dimensional array
 b = np.array([1, 2, 3, 4, 5]) #1 dimantional array
 c = np.array([[1, 2, 3], [4, 5, 6]]) #2 dimantional array 
 d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]]) #3 dimantioal array
 e = np.array([1, 2, 3, 4], ndmin=6) #n dimantional array
 
+
 print('---------------------------------------')
 print("Zero dimensional array")
-print(a.ndim) 
-print(a)        # print a array
+print(a.ndim)   # print dimension of array
+print(a)        # print array
 print('---------------------------------------')
 print("One dimensional array")
 print(b.ndim)  
-print(b)        # print b array
+print(b)       
 print('---------------------------------------')
 print("Two dimensional array")
 print(c.ndim)  
-print(c)        # print c array
+print(c)       
 print('---------------------------------------')
 print("Three dimensional array")
 print(d.ndim)  
-print(d)        # print d array
+print(d)       
 print('---------------------------------------')
 print("N dimensional array")
 print(e.ndim) 
-print(e)        # print e array
+print(e)      
 print('---------------------------------------')
 
 

5fc43ed32087fdbf2fff78c0cac677495fa5cd66
Amit Amrutiya <90676830+20CS003@users.noreply.github.com>
2022-04-04 22:58:15 +0530

    Update create_array.py

--- create_array.py
+++ create_array.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 # create array 
-a = np.array(42)  #0 dimensional array
-b = np.array([1, 2, 3, 4, 5]) #1 dimantional array
-c = np.array([[1, 2, 3], [4, 5, 6]]) #2 dimantional array 
-d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]]) #3 dimantioal array
-e = np.array([1, 2, 3, 4], ndmin=6) #n dimantional array
+a = np.array(42)                                                #0 dimensional array
+b = np.array([1, 2, 3, 4, 5])                                   #1 dimantional array
+c = np.array([[1, 2, 3], [4, 5, 6]])                            #2 dimantional array 
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])  #3 dimantional array
+e = np.array([1, 2, 3, 4], ndmin=6)                             #n dimantional array
 
 
 print('---------------------------------------')
